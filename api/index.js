072a2d8e198d6b793416ccfcd03aaf31ffc928b1
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 09:50:54 -0300

    chore: update shareCode to F2QCALRJ10N

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=I62UN14018Q&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=F2QCALRJ10N&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
