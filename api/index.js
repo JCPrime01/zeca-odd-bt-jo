36d7bd16534c2820fbcf1eef4a7190103d7e99c5
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 10:58:58 -0300

    chore: update shareCode to 7KL1QQTJO6H

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=6Z8C993HB59&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=7KL1QQTJO6H&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
