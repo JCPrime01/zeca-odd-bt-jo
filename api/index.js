18b72bec781a54e77e5d85fa9df5a1bf49bb43d2
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-26 10:36:47 -0300

    chore: update shareCode to 77L3CHC4677

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=8TOZH93IX2X&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=77L3CHC4677&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
