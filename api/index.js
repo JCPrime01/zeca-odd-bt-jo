57986491f873ab8e883c7d30636c25c26a0b50d7
JCPrime01 <vanzokiller@gmail.com>
2026-05-31 10:34:15 -0300

    chore: shareCode F7CX0PF7UAA -> 75DDT7CQ8Q9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=F7CX0PF7UAA&afp8=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/2wo6uo64?shareCode=75DDT7CQ8Q9&afp8=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
