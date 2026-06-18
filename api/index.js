5bef7deb6579ae228666a2db05f49b6acbfcf0ce
JCPrime01 <vanzokiller@gmail.com>
2026-06-18 10:20:45 -0300

    chore: shareCode 5XXPQSZFLCC -> ILKQUW0MMWK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=5XXPQSZFLCC&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=ILKQUW0MMWK&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
