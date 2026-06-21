ca60f2d115646757242f6aa3beec2812c0f0e36c
JCPrime01 <vanzokiller@gmail.com>
2026-06-21 11:00:17 -0300

    chore: shareCode DG9XWWCALYP -> AU5QURJXVWY (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=DG9XWWCALYP&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=AU5QURJXVWY&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
