cd003ecd613976a63fdddbe0a81d79daa0a9650f
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 10:47:22 -0300

    chore: shareCode PMTGZRNZIUP -> JN2ISOI3S9M (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=PMTGZRNZIUP&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=JN2ISOI3S9M&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
