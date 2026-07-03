5d4033801c3b081b5765fdf4acfd76edacc16e0b
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 09:33:01 -0300

    chore: shareCode 9YLN3IW5V6E -> ZTJL1VB7SQV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=9YLN3IW5V6E&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=ZTJL1VB7SQV&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
