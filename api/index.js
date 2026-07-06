570a02538783ee2aaa7488cd01a330d1c0bc3748
JCPrime01 <vanzokiller@gmail.com>
2026-07-06 12:03:17 -0300

    chore: shareCode N2DM1VZZATO -> AL2TQ21O3NT (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=N2DM1VZZATO&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=AL2TQ21O3NT&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
