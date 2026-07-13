783f2430d83dc669ab981c5ba5899e80be0aa7c3
JCPrime01 <vanzokiller@gmail.com>
2026-07-13 09:32:33 -0300

    chore: shareCode 8EWRB5UF543 -> SUTPDV9HPT7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=8EWRB5UF543&afp2=sb&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/aibq3qwm?campaign_id=23426&shareCode=SUTPDV9HPT7&afp2=sb&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
